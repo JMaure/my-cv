3a3b3e53bd6b1fcfa46eff92c148a4a18e45e502
Julien <julienmaure.pro@gmail.com>
2024-10-17 19:19:56 +0200

    complete acceuil page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,20 @@
-<h1>Julien Mauré</h1>
+<script>
+    import './accueil.css';
+    import portraitCV from '$lib/images/portraitCV.png';
+</script>
 
-<h2>Bienvenu sur mon site</h2>
+<h1>Bienvenue sur mon site</h1>
+
+<div class="main">
+    <div class="portrait">
+        <img src= {portraitCV} alt="Portait CV">
+    </div>
+    <div class="infos">
+        <h2>Julien Mauré Développeur Full Stack</h2>
+        <p>Diplômé d'une licence APSIO avec un an d'expérience dans une ESN, 
+            j'ai par la suite  développé de nouvelles compétences dans un autre secteur d'activé. 
+            Avec pour projet de trouver un poste dans le développement informatique, 
+            je viens de terminer une POE pour mettre à jour mes connaissances et en acquérir de nouvelles. </p>
+
+    </div>
+</div>
